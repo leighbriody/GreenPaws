f231f1d9e25811a6c197f6cbadd0982ba33c306c
leigh <lbriody10@gmail.com>
2023-09-01 08:23:05 +0100

    basic next auth added

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.imgur.com",
+      },
+    ],
+  },
+  env: {
+    GITHUB_APP_CLIENT_ID: "Iv1.498282d92b4b0dc4",
+    GITHUB_APP_CLIENT_SECRET: "251d06527172fa0c686bf0d6ffcf3e1531dd54f2",
+    NEXTAUTH_SECRET: "aWgJ9qDhBgezSeEr7QD8AwKpbW",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
