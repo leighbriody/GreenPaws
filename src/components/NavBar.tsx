bc009c8b8a6221943261a0aa4b0760ec10cdfba3
leigh <lbriody10@gmail.com>
2023-10-18 21:04:25 +0100

    update dashboard to theme

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -122,7 +122,7 @@ export default function NavBar() {
                 }`}
                 href={"/dashboard/profile"}
               >
-                Dashboard
+                {theme}
               </Link>
             )}
 
